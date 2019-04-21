75bb09e39b7b25f22ea7460aa7c896045bf1d204
Tullia Padellini <tullia.padellini@uniroma1.it>
2019-04-21 10:04:31 +0200

    Update test1.R

--- test1.R
+++ test1.R
@@ -13,3 +13,11 @@ cc = create.palette(prova, aggregate = "mean")
 dd = create.palette(prova,11, "median")
 plot(1:4, rep(0,4), col = cc, pch = 20, cex = 4, axes = F, xlab = "", ylab = "")
 points(seq(1, 4, length.out = 11), rep(0.2,11), col = dd, pch = 20, cex = 4)
+
+
+
+col1 = get.colors("Alessandro")
+plot(1:22, rep(0, 22), pch = 15, col = col1, cex = 4, axes = F, xlab = "", ylab = "")
+
+col2 = get.colors("Tullia")
+points(1:22, rep(-0.2, 22), pch = 15, col = col2, cex = 4)
